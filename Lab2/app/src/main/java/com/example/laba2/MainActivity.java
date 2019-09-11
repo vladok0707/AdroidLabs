535a8b3dbb0c4243f084a4f2decaa73a7289ba20
vladok0707 <31615922+vladok0707@users.noreply.github.com>
2019-09-11 22:54:41 +0300

    end

--- Lab2/app/src/main/java/com/example/laba2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/laba2/MainActivity.java
@@ -6,21 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener  {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private int selecretitemradio = 0;
-    private int selecretitemactyvity = 0;
-    private Button mButton;
     private Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mButton = findViewById(R.id.OK);
-        mButton.setOnClickListener(this);
         spinner = (Spinner) findViewById(R.id.activity_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.actyvity_array, android.R.layout.simple_spinner_item);
@@ -31,29 +30,68 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     public void onRadioButtonClicked(View view) {
         // если переключатель отмечен
         boolean checked = ((RadioButton) view).isChecked();
-
         // Получаем нажатый переключатель
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.Male:
-                if (checked){
+                if (checked) {
                     selecretitemradio = 0;
                 }
                 break;
             case R.id.Female:
-                if (checked){
+                if (checked) {
                     selecretitemradio = 1;
                 }
                 break;
         }
     }
-    @Override
-    public void onClick(View view)
-    {
+
+    public void onClick(View view) {
         switch (view.getId()) {
             case R.id.OK:
+                double BMR;
+                EditText first = (EditText) findViewById(R.id.age);
+                EditText second = (EditText) findViewById(R.id.weight);
+                EditText third = (EditText) findViewById(R.id.height);
                 TextView res = (TextView) findViewById(R.id.result);
-                res.setText(selecretitemradio + " : " + spinner.getSelectedItemId());
+                if (selecretitemradio == 1) {
+                    BMR = 655.0955 + (9.5634 * ParseDouble(second))
+                            + (1.8496 * ParseDouble(third))
+                            - (4.6756 * ParseDouble(first));
+                } else {
+                    BMR = 66.4730 + (13.7516 * ParseDouble(second))
+                            + (5.0033 * ParseDouble(third))
+                            - (6.7550 * ParseDouble(first));
+                }
+                res.setText("Ваша суточная норма (Ккал): " + BMR * SpinnerX(spinner));
         }
     }
 
+
+    private double ParseDouble(EditText e) {
+        double res=0;
+        try {
+            res = Double.parseDouble(e.getText().toString());
+        } catch (Exception ex) {
+            //Toast.makeText(this, "Not correct data, return 0", Toast.LENGTH_SHORT).show();
+            e.setText("0");
+            return 0;
+        }
+
+        return res;
+    }
+
+
+    private double SpinnerX(Spinner s) {
+        switch (s.getId()) {
+            case 0:
+                return 1.2;
+            case 1:
+                return 1.55;
+            case 2:
+                return 1.9;
+            default: return 1.2;
+        }
+
+
+    }
 }
