086fcb033919647c6d72f754c6deaa507356f7d2
vladok0707 <vladok-warface@mail.ru>
2019-09-19 11:10:08 +0300

    laba4 update filter

--- Laba4/app/src/main/java/com/example/laba4/MainActivity.java
+++ Laba4/app/src/main/java/com/example/laba4/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 Intent intent = new Intent(getApplication(), Main2Activity.class);
-                intent.putExtra(Contact.class.getSimpleName(), contacts.get(position));
+                intent.putExtra(Contact.class.getSimpleName(),contacts.get(position));
                 startActivity(intent);
             }
         });
